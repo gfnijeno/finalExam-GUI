799b4951f54ff5ddbc09d8589c99a40c34f43c47
Mureigne Leivin Y. Daet <111488534+gfnijeno@users.noreply.github.com>
2023-03-06 14:26:07 +0800

    Update ForgotPassword.java

--- ForgotPassword.java
+++ ForgotPassword.java
@@ -19,10 +19,11 @@ import javax.swing.JSeparator;
 import java.awt.SystemColor;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 public class ForgotPassword {
 
-	public JFrame frame;
+	public JFrame frmForgotPassword;
 	private JTextField email;
 	/**
 	 * Launch the application.
@@ -32,7 +33,7 @@ public class ForgotPassword {
 			public void run() {
 				try {
 					ForgotPassword window = new ForgotPassword();
-					window.frame.setVisible(true);
+					window.frmForgotPassword.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -51,80 +52,82 @@ public class ForgotPassword {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(255, 255, 255));
-		frame.setBounds(100, 100, 542, 365);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmForgotPassword = new JFrame();
+		frmForgotPassword.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\murei\\Downloads\\386381954011578368.png"));
+		frmForgotPassword.setTitle("Forgot Password");
+		frmForgotPassword.getContentPane().setBackground(new Color(255, 255, 255));
+		frmForgotPassword.setBounds(100, 100, 542, 365);
+		frmForgotPassword.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmForgotPassword.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Password Reset");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(0, 114, 528, 25);
-		frame.getContentPane().add(lblNewLabel);
+		frmForgotPassword.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Enter your Spotify username, or the email address that you");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel_1.setBounds(47, 149, 434, 14);
-		frame.getContentPane().add(lblNewLabel_1);
+		frmForgotPassword.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("used to register. We'll send you an email with your username");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setBounds(47, 165, 434, 14);
-		frame.getContentPane().add(lblNewLabel_2);
+		frmForgotPassword.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("and a link to reset your password.");
 		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2_1.setBounds(47, 179, 434, 14);
-		frame.getContentPane().add(lblNewLabel_2_1);
+		frmForgotPassword.getContentPane().add(lblNewLabel_2_1);
 		
 		JLabel lblNewLabel_3 = new JLabel("Email address or username");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 10));
 		lblNewLabel_3.setBounds(124, 213, 140, 14);
-		frame.getContentPane().add(lblNewLabel_3);
+		frmForgotPassword.getContentPane().add(lblNewLabel_3);
 		
 		email = new JTextField();
 		email.setBounds(124, 228, 280, 20);
-		frame.getContentPane().add(email);
+		frmForgotPassword.getContentPane().add(email);
 		email.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Send");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(email.getText().isEmpty()) {
-				JOptionPane.showMessageDialog(null, "You must enter a valid email!");
+				JOptionPane.showMessageDialog(null, "You must enter a valid email or username!");
 				} else {
 					JOptionPane.showMessageDialog(null, "We've sent you an email. Just follow the instructions to reset your password.");
 				}
 			}
 		});
 		btnNewButton.setBounds(146, 267, 82, 25);
-		frame.getContentPane().add(btnNewButton);
+		frmForgotPassword.getContentPane().add(btnNewButton);
 		
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				spotify sp = new spotify();
 				sp.Login.setVisible(true);
-				frame.dispose();
+				frmForgotPassword.dispose();
 			}
 		});
 		btnBack.setBounds(299, 267, 82, 25);
-		frame.getContentPane().add(btnBack);
+		frmForgotPassword.getContentPane().add(btnBack);
 		
 		JLabel lblNewLabel_4 = new JLabel("");
 		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\murei\\Downloads\\Spotify_Logo_CMYK_Black (1).png"));
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_4.setBounds(127, 0, 273, 87);
-		frame.getContentPane().add(lblNewLabel_4);
+		frmForgotPassword.getContentPane().add(lblNewLabel_4);
 		
 		JSeparator separator = new JSeparator();
 		separator.setBackground(SystemColor.menu);
 		separator.setBounds(-176, 89, 880, 8);
-		frame.getContentPane().add(separator);
+		frmForgotPassword.getContentPane().add(separator);
 	}
 
 }
